18046c1f5e021445e09284d02a82897cc34ee6fc
Jia Chen <36725840+jiaChenCloseParenthesesColon@users.noreply.github.com>
2018-11-24 20:29:11 +0800

    Initial Commit

---
+++ SCS hackathonTests/SCS_hackathonTests.swift
@@ -0,0 +1,34 @@
+//
+//  SCS_hackathonTests.swift
+//  SCS hackathonTests
+//
+//  Created by JiaChen(: on 24/11/18.
+//  Copyright © 2018 SST Inc. All rights reserved.
+//
+
+import XCTest
+@testable import SCS_hackathon
+
+class SCS_hackathonTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
